447f41bb82484475cd8cfe9fd1e336f0738eb338
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-04 16:25:21 +0000

    Adds complete working "fizzBuzz" function

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,7 +1,19 @@
 describe("fizzBuzz", function() {
 
-  it("should return 'fizz' when given 3", function() {
-    expect(fizzBuzz(3)).toEqual("fizz")
+  it("should return 'fizz' when given a number divisible by 3", function() {
+    expect(fizzBuzz(3)).toEqual("fizz");
+  });
+
+  it("should return 'buzz' when given a number divisible by 5", function() {
+    expect(fizzBuzz(5)).toEqual("buzz");
+  });
+
+  it("should return 'fizzBuzz' when given a number divisible by 15", function() {
+    expect(fizzBuzz(15)).toEqual("fizzBuzz");
+  });
+
+  it("should return the given number when it is not divisible by 3 or 5", function() {
+    expect(fizzBuzz(1)).toEqual(1);
   });
 
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,11 @@
 function fizzBuzz(num) {
-  if (num % 3 === 0) {
-    return 'fizz'
-  };
-};
+  if (num % 15 === 0) {
+    return 'fizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
+}
